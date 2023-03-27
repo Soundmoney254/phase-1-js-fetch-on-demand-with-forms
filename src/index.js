e6b67d360e1e089095762b0643fe8498ff00210b
Samuel <samnmbuguah@gmail.com>
2023-03-27 16:34:29 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const init = () => {
-  
-}
-
 document.addEventListener('DOMContentLoaded', init);
+function init() {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    console.log(searchByID.value);
+    
+    fetch(`http://localhost:3000/movies/${searchByID.value}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    })
+    });
+}
